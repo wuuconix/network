bb5e41dd5769aed48b6669968ce403e0744b527d
wuuconix <1521900139@qq.com>
2021-12-01 13:46:58 +0800

    add lab2-1-2

--- lab1-2-1/client.c
+++ lab1-2-1/client.c
@@ -6,13 +6,11 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-// 客户端从标准输入输入字符串和从标准输出输出字符串
 void str_cli(FILE *fp, int sockfd){
 
-    char sendbuf[255] = {0}; // 发送buf
-    char recvbuf[255] = {0}; // 接受的buf
-    // 循环输入和输出
-    while(fgets(sendbuf, sizeof(sendbuf), fp) != NULL)
+    char sendbuf[255] = {0}; // 发送缓冲区
+    char recvbuf[255] = {0}; // 接受缓冲区
+    while(fgets(sendbuf, sizeof(sendbuf), fp) != NULL)  //循环从stdin读入 向stdin输出
     {
         write(sockfd,sendbuf, sizeof(sendbuf));
         read(sockfd,recvbuf, sizeof(recvbuf));
@@ -22,19 +20,25 @@ void str_cli(FILE *fp, int sockfd){
     }
 
 }
-// 客户端的主程序
+
 int main()
 {
     int sockfd;
     struct sockaddr_in servaddr;
-    //指定通信协议理性
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("create socket error!\n");
+        return 0;
+    }
     bzero(&servaddr,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(9877);//端口号
-    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);//设置服务器ip
-    connect(sockfd, (struct sockaddr*)&servaddr,sizeof(servaddr));//进行tcp连接
+    servaddr.sin_port = htons(9877); //端口号
+    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr); //设置服务器ip
+    if (connect(sockfd, (struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+    {
+        printf("connect to server error!\n");
+        return 0;
+    }
     str_cli(stdin, sockfd);
-    exit(0);
-
+    return 0;
 }
--- lab1-2-1/server.c
+++ lab1-2-1/server.c
@@ -10,45 +10,42 @@
 
 // 服务器从网络读入和返回
 
-void str_echo(int sockfd) {
+void str_back(int sockfd)
+{
     ssize_t n;
     char buf[255] = {0};
     while(1) {
-        n = read(sockfd,buf,sizeof(buf));
+        n = read(sockfd, buf, sizeof(buf)); //raed函数的返回值是ssize_t
         if(n == 0)
             break;
         write(sockfd, buf, n);
-        // bzero(buf,sizeof(buf));
+        printf("recv:\n%ssend back to clinet yet!\n", buf);
+        bzero(buf, sizeof(buf));
     }
 }
-int main() {
+int main() 
+{
     int listenfd, connfd;
-    socklen_t clilen;
-    pid_t child_pid;
     struct sockaddr_in servaddr;
-    struct sockaddr_in cliaddr;
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("create a listen socket error!\n");
+    }
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(9877);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    //设置最大连接数目
     listen(listenfd, 5);
 
-    while(1) {
-        clilen = sizeof(clilen);
-        connfd = accept(listenfd, (struct sockaddr *)&cliaddr,&clilen);   //创建子进程 进行读入和返回
-        printf("connected : connfd = %d\n", connfd);
-        if((child_pid = fork()) == 0) {
-            close(listenfd); //在子进程中关闭父进程的监听套接字
-            str_echo(connfd);
-            exit(0);
-        }
+    while(1) //循环服务器  处理好一个连接后 断开 接受另外一个连接。
+    {
+        connfd = accept(listenfd, (struct sockaddr *)NULL, NULL);   //创建子进程 进行读入和返回
+        printf("a client connect me!\n");
+        str_back(connfd);
         close(connfd);   //父进程中关闭已连接套接字
     }
 
-    close(listenfd);
     return 0;
 }
