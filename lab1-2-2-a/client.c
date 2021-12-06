3cc9c7d782a13394d5760a32a9eb10569ca717bc
wuuconix <1521900139@qq.com>
2021-12-06 23:53:52 +0800

    make lab1-2-2-a final done

--- lab1-2-2-a/client.c
+++ lab1-2-2-a/client.c
@@ -5,59 +5,48 @@
 #include <strings.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
 #define BUFSIZE 1024
+#define PORT 23333
 
 void str_cli(FILE *fp, int sockfd, int n_recv){
 
     char sendbuf[BUFSIZE]; // 发送缓冲区
-    char recvbuf[BUFSIZE]; // 接受缓冲区
-    char printbuf[BUFSIZ]; // 输出缓冲区
+    char *recvbuf = (char*)malloc(sizeof(char) * (n_recv + 1));
+    char wastebuf[65535];
     while(fgets(sendbuf, sizeof(sendbuf), fp) != NULL)  //循环从stdin读入 向stdin输出
     {
+        sendbuf[strlen(sendbuf) - 1] = 0; //不发送换行, 因为换行不算有效数据
         write(sockfd, sendbuf, sizeof(sendbuf));
-        read(sockfd, recvbuf, sizeof(recvbuf)); //接收字符不能在read第三个参数直接改，否则缓冲区实际长度和第三个参数不匹配会发生奇怪现象
-        char c = recvbuf[0];
-        int index = 0;
-        while ((int)c != 0 && index < n_recv) //将recvbuf的值搬到printbuf中
-        {
-            printbuf[index] = recvbuf[index];
-            index++;
-            c = recvbuf[index];
-        }
-        if ((int)printbuf[index - 1] != 10) //若最后没有换行
-        {
-            printbuf[index] = 10;
-            printbuf[index + 1] = 0;
-        }
-        else
-            printbuf[index] = 0;
-        printf("%s", printbuf);
+        read(sockfd, recvbuf, n_recv); //只读有效的，\0不读，recvbuf初始都是0
+        read(sockfd, wastebuf, sizeof(wastebuf)); //将sockfd缓冲区剩余的读入wastebuf丢弃
+        printf("%s\n", recvbuf);
+        printf("--------------\n");
         bzero(sendbuf, sizeof(sendbuf));
-        bzero(recvbuf, sizeof(recvbuf));
-        bzero(printbuf, sizeof(printbuf));
+        bzero(recvbuf, n_recv + 1);
     }
 
 }
 
 int main()
 {
-    int n_recv = 5; //每次recv的长度
+    int n_recv; //每次recv的长度
     printf("please input n to decide how long to receive: ");
     scanf("%d", &n_recv);
     fgetc(stdin);
 
     int sockfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in serveraddr;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("create socket error!\n");
         return 0;
     }
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(23333); //端口号
-    inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr); //设置服务器ip
-    if (connect(sockfd, (struct sockaddr*)&servaddr,sizeof(servaddr)) < 0)
+    bzero(&serveraddr,sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT); //端口号
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //设置服务器ip
+    if (connect(sockfd, (struct sockaddr*)&serveraddr,sizeof(serveraddr)) < 0)
     {
         printf("connect to server error!\n");
         return 0;
--- lab1-2-2-a/server.c
+++ lab1-2-2-a/server.c
@@ -7,61 +7,54 @@
 #include <netinet/in.h>
 #include <sys/wait.h>
 #include <errno.h>
-#define BUFSIZE 1024
+#define PORT 23333
 
-void str_back(int sockfd, int n_recv)
+void str_echo(int sockfd, int n_recv)
 {
-    char recvbuf[BUFSIZE];
-    char sendbuf[BUFSIZE];
+    char *sendbuf = (char*)malloc(sizeof(char) * (n_recv + 1)); //多的一个用来存\0
+    char *recvbuf = (char*)malloc(sizeof(char) * (n_recv + 6)); //相比sendbuf多了ECHO: 5个字符
+    char wastebuf[65535];
     while(1) 
     { 
-        bzero(sendbuf, sizeof(sendbuf));
-        bzero(recvbuf, sizeof(recvbuf));
-
-        int n = read(sockfd, recvbuf, sizeof(recvbuf));
+        int n = read(sockfd, recvbuf, n_recv);
         if(n == 0)
             break;
-        sendbuf[0] = 'E', sendbuf[1] = 'C', sendbuf[2] = 'H', sendbuf[3] = 'O', sendbuf[4] = ':'; //实验题目要求加ECHO:
-        char c = recvbuf[0];
-        int index = 0;
-        while ((int)c != 0 && index < n_recv) //将recvbuf的值搬到sendbuf中
-        {
-            sendbuf[index + 5] = recvbuf[index];
-            index++;
-            c = recvbuf[index];
-        }
-        if ((int)sendbuf[index + 5 - 1] != 10) //若最后没有换行（输入的字符长度大于n_recv的情况手动添加换行
-            sendbuf[index + 5] = 10;
-        
-        write(sockfd, sendbuf, sizeof(sendbuf));
+        printf("%s\n", recvbuf);
+        read(sockfd, wastebuf, sizeof(wastebuf)); //将sockfd缓冲区剩余的读入wastebuf丢弃
+        strcpy(sendbuf, "ECHO:"); //实验题目要求加ECHO:
+        strcpy(sendbuf + 5, recvbuf);
+        write(sockfd, sendbuf, n_recv + 6);
+        bzero(sendbuf, n_recv + 1);
+        bzero(recvbuf, n_recv + 6);
     }
 }
 int main() 
 {
-    int n_recv = 5; //每次recv的长度
+    int n_recv; //每次recv的长度
     printf("please input n to decide how long to receive: ");
     scanf("%d", &n_recv);
     fgetc(stdin);
 
     int listenfd, connfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in serveraddr;
 
     if((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("create a listen socket error!\n");
     }
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(23333);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    bzero(&serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(PORT);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
     listen(listenfd, 5);
 
     while(1) //循环服务器  处理好一个连接后 断开 接受另外一个连接。
     {
-        connfd = accept(listenfd, (struct sockaddr *)NULL, NULL);   //创建子进程 进行读入和返回
+        connfd = accept(listenfd, (struct sockaddr *)NULL, NULL);
         printf("a client connect me!\n");
-        str_back(connfd, n_recv);
+        str_echo(connfd, n_recv);
         close(connfd);   //父进程中关闭已连接套接字
         printf("a client disconnect me!\n");
     }
